0165abb5a36b11d93dce6d898016b1e16fe51683
Foenix <lzy1998@mail.ustc.edu.cn>
2019-05-04 13:14:19 +0800

    create sudukuMap.h

---
+++ Lab1/sudoku/sudokuMap.h
@@ -0,0 +1,99 @@
+#ifndef SUDOKUMAP_H
+#define SUDOKUMAP_H
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <random>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+fstream fileName;
+int n, k;
+mt19937 rng;
+
+class Map{
+public:
+    int Sudoku[20][20];
+    //更新哪些位置不能更改
+    void upgrade(){
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (Sudoku[i][j]) unChanged[i][j] = true;
+    }
+    //随机写入数据，保证每行不存在重复元素
+    void writeMap(){
+        bool flag[20];
+        int cnt = 1;
+        for (int i = 0; i < n; i++){
+            memset(flag, 0, sizeof(flag));
+            for (int j = 0; j < n; j++)
+                if (unChanged[i][j]) flag[Sudoku[i][j]] = true;
+            for (int j = 0; j < n; j++){
+                if (!unChanged[i][j]){
+                    int tmp = uniform_int_distribution<int>(1, k)(rng);
+                    while(flag[tmp]){
+                        tmp = uniform_int_distribution<int>(1, k)(rng);
+                    }
+                    Sudoku[i][j] = tmp;
+                    flag[tmp] = true;
+                }
+            }
+        }
+    }
+    void Print(){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++)
+                cout << Sudoku[i][j] << " ";
+            cout << endl;
+        }
+    }
+    void PrintFile(){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++)
+                fileName << Sudoku[i][j] << " ";
+            fileName << endl;
+        }
+    }
+    //随机交换某行中的两个可更改的数据
+    //保证该行中不会出现重复
+    Map operatorMap(){
+        int col, row1, row2;
+        Map ret;
+        while(true){
+            col = uniform_int_distribution<int>(0, n-1)(rng);
+            row1 = uniform_int_distribution<int>(0, n-1)(rng);
+            row2 = uniform_int_distribution<int>(0, n-1)(rng);
+            if (row1 == row2)continue;
+            if ((!unChanged[col][row1]) && (!unChanged[col][row2]))
+                break;
+        }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                ret.Sudoku[i][j] = Sudoku[i][j], ret.unChanged[i][j] = unChanged[i][j];
+        swap(ret.Sudoku[col][row1], ret.Sudoku[col][row2]);
+        return ret;
+    }
+    //评估函数，每列中重复元素个数，不用考虑每行，随机填充时保证每行不存在重复元素
+    int H(){
+        int ret = 0;
+        bool flag[20];
+        for (int i = 0; i < n; i++){
+            memset(flag, 0, sizeof(flag));
+            for (int j = 0; j < n; j++){
+                if (!flag[Sudoku[j][i]]) flag[Sudoku[j][i]] = true;
+                else ret++;
+            }
+        }
+        return ret;
+    }
+private:
+    //外界不可更改
+    //保存哪些位置不可更改
+    bool unChanged[20][20];
+};
+
+#endif
--- Lab1/sudoku/sudoku_simulated_annealing.cpp
+++ Lab1/sudoku/sudoku_simulated_annealing.cpp
@@ -5,85 +5,10 @@
 #include <random>
 #include <ctime>
 #include <cmath>
+#include "sudokuMap.h"
 
 using namespace std;
-fstream fileName;
-int n, k;
-mt19937 rng;
 
-class Map{
-public:
-    int Sudoku[20][20];
-    void upgrade(){
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (Sudoku[i][j]) unChanged[i][j] = true;
-    }
-    void writeMap(){
-        bool flag[20];
-        int cnt = 1;
-        for (int i = 0; i < n; i++){
-            memset(flag, 0, sizeof(flag));
-            for (int j = 0; j < n; j++)
-                if (unChanged[i][j]) flag[Sudoku[i][j]] = true;
-            for (int j = 0; j < n; j++){
-                if (!unChanged[i][j]){
-                    int tmp = uniform_int_distribution<int>(1, k)(rng);
-                    while(flag[tmp]){
-                        tmp = uniform_int_distribution<int>(1, k)(rng);
-                    }
-                    Sudoku[i][j] = tmp;
-                    flag[tmp] = true;
-                }
-            }
-        }
-    }
-    void Print(){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++)
-                cout << Sudoku[i][j] << " ";
-            cout << endl;
-        }
-    }
-    void PrintFile(){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++)
-                fileName << Sudoku[i][j] << " ";
-            fileName << endl;
-        }
-    }
-    Map operatorMap(){
-        int col, row1, row2;
-        Map ret;
-        while(true){
-            col = uniform_int_distribution<int>(0, n-1)(rng);
-            row1 = uniform_int_distribution<int>(0, n-1)(rng);
-            row2 = uniform_int_distribution<int>(0, n-1)(rng);
-            if (row1 == row2)continue;
-            if ((!unChanged[col][row1]) && (!unChanged[col][row2]))
-                break;
-        }
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                ret.Sudoku[i][j] = Sudoku[i][j], ret.unChanged[i][j] = unChanged[i][j];
-        swap(ret.Sudoku[col][row1], ret.Sudoku[col][row2]);
-        return ret;
-    }
-    int H(){
-        int ret = 0;
-        bool flag[20];
-        for (int i = 0; i < n; i++){
-            memset(flag, 0, sizeof(flag));
-            for (int j = 0; j < n; j++){
-                if (!flag[Sudoku[j][i]]) flag[Sudoku[j][i]] = true;
-                else ret++;
-            }
-        }
-        return ret;
-    }
-private:
-    bool unChanged[20][20];
-};
 Map initMap;
 
 int fileIO(string inputFile){
@@ -100,12 +25,13 @@ int fileIO(string inputFile){
     fileName.close();
     return 0;
 }
-
+//模拟退火
 int simulated_annealing(string outputFile){
     fileName.open(outputFile);
     if (!fileName.is_open()){
         return -1;
     }
+    //设置初始温度，最低温度，温度跨度
     double T = 10000;
     double T_min = 0.01;
     double r = 0.9999;
@@ -120,7 +46,6 @@ int simulated_annealing(string outputFile){
             X0 = X1;
         }
         else if (exp((X0.H() - X1.H()) / T) > 0.9){
-                    //(double)uniform_int_distribution<int>(0, 1000)(rng) / 1000){
             X0 = X1;
         }
         T = T * r;
@@ -137,10 +62,12 @@ int main(int argc, char * argv[]){
         printf("Paraments Error\n");
         return 0;
     }
+    //读文件
     if (fileIO(argv[1]) == -1){
         printf("Open File Fall\n");
         return 0;
     }
+    //模拟退火
     if (simulated_annealing(argv[2]) == -1){
         printf("Open File Faill\n");
         return 0;
